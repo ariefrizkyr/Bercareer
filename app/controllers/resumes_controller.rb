d3542337868c68b8fa15fc598b8d20aa2fde9675
Arief R Ramadhan <ariefrizkyr@gmail.com>
2016-03-04 19:40:56 +0700

    Fixed bugs in resume

--- app/controllers/resumes_controller.rb
+++ app/controllers/resumes_controller.rb
@@ -1,37 +1,29 @@
 class ResumesController < ApplicationController
-  before_action :find_resume, only: [:show, :edit, :update]
-  before_action :authenticate_student!, except: [:index, :show]
-  before_action :only_current_student, except: [:index, :show]
-
-  def index
-    @resume = Resume.all.order("Created_at DESC")
-  end
-
-  def show
-
-  end
+  before_action :find_resume, only: [:edit, :update]
+  before_action :authenticate_student!
+  before_action :only_current_student
 
   def new
-    @resume = current_student.build_resume
+    @resume = Resume.new
   end
 
   def create
     @resume = current_student.build_resume(resume_params)
 
     if @resume.save
-      redirect_to student_resume_path(current_student), success: "Resume created!"
+      redirect_to student_path(params[:student_id]), success: "Resume created!"
     else
       render 'new'
     end
   end
 
   def edit
-
+    @resume = Resume.find(params[:student_id])
   end
 
   def update
     if @resume.update(resume_params)
-      redirect_to student_resume_path(current_student), success: "Resume updated!"
+      redirect_to student_path(params[:student_id]), success: "Resume updated!"
     else
       render 'edit'
     end
@@ -47,7 +39,7 @@ class ResumesController < ApplicationController
                                      :city, :province, :country, :postal_code, :university,
                                      :major, :level, :grad_year, :gpa, :facebook_url,
                                      :twitter_url, :linkedin_url, :googleplus_url,
-                                     skill_list: [], language_list: [])
+                                     skill_list:[], language_list:[])
     end
 
     def only_current_student
