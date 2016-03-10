7f02658d72c6af3e4aa750a304910db6827ad2d3
Arief R Ramadhan <ariefrizkyr@gmail.com>
2016-03-10 18:58:03 +0700

    Improved devise params sanitizer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,7 +19,7 @@ class ApplicationController < ActionController::Base
     end
 
     def configure_permitted_parameters
-      devise_parameter_sanitizer.for(:sign_up) << :full_name << :nim << :company_name << :phone_number
-      devise_parameter_sanitizer.for(:account_update) << :full_name << :nim << :company_name << :phone_number
+      devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:full_name, :nim, :email, :password, :password_confirmation, :company_name, :phone_number) }
+      devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:full_name, :nim, :email, :password, :password_confirmation,:company_name, :phone_number) }
     end
 end
