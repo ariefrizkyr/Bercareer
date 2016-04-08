92509740b32a0dcf5939495e7c171cc3732acea7
Arief R Ramadhan <ariefrizkyr@gmail.com>
2016-04-08 22:59:38 +0700

    Improved job and aand applicant list page

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,7 +1,7 @@
 class Profile < ActiveRecord::Base
   belongs_to :company
 
-  has_attached_file :logo, styles: { medium: "300x300>", thumb: "100x100>" }
+  has_attached_file :logo, styles: { medium: "300x300>", small: "150x150>", thumb: "100x100>" }
   validates_attachment_content_type :logo, content_type: /\Aimage\/.*\Z/
 
   validates :address, presence: true
