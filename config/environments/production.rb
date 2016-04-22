be2e6247db17a86694488a5ae911272129c5032d
Arief R Ramadhan <ariefrizkyr@gmail.com>
2016-04-22 13:06:46 +0700

    Fixed typo in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,13 +81,13 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'bercareer.com' }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.default charset: "utf-8"
 
   # Use smtp for sending mail
   config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.smtp_settings = {
-    domain: 'bercareer.com'
+    domain: 'bercareer.com',
     address: ENV['SENGRID_URL'],
     port: 587,
     enable_starttsl_auto: true,
