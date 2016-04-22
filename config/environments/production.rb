a84fecf88c8443d9c2297ecca70f2c0b82e26127
Arief R Ramadhan <ariefrizkyr@gmail.com>
2016-04-22 13:01:44 +0700

    Fixed typo in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,7 +81,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'bercareer.com' }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default charset: "utf-8"
+  config.action_mailer.default :charset => "utf-8"
 
   # Use smtp for sending mail
   config.action_mailer.delivery_method = :smtp
