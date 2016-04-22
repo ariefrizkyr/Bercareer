f2e8d09164fbedfe7f6a61121ab77d4d1a76594f
Arief R Ramadhan <ariefrizkyr@gmail.com>
2016-04-22 20:42:36 +0700

    Improved contact redirection

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -8,6 +8,7 @@ class ContactsController < ApplicationController
     @contact.request = request
     if @contact.deliver
       flash.now[:success] = "Message sent. We'll be in touch as soon as possible."
+      redirect_to new_contact_path
     else
       flash.now[:error] = "Error occured. Can't send the message."
       render :new
