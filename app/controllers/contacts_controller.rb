fa6dc7e8895c2489dc953a3a35132b77160e08ef
Arief R Ramadhan <ariefrizkyr@gmail.com>
2016-04-23 08:28:36 +0700

    Modified contacts controller

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -7,10 +7,10 @@ class ContactsController < ApplicationController
     @contact = Contact.new(params[:contact])
     @contact.request = request
     if @contact.deliver
-      flash.now[:success] = "Message sent. We'll be in touch as soon as possible."
-      render :new
+      flash[:success] = "Message sent. We'll be in touch as soon as possible."
+      redirect_to new_contact_path
     else
-      flash.now[:error] = "Error occured. Can't send the message."
+      flash[:error] = "Error occured. Can't send the message."
       render :new
     end
   end
